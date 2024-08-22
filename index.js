e951cfd4381a984bc983433d24fb8b0840adf7ce
Thiago Silva <thiagogs@br.ibm.com>
2024-08-22 12:28:47 -0300

    add the support for versioning the controllers

--- index.js
+++ index.js
@@ -1,173 +1,193 @@
 const Express = require('express');
 
 class IkeRouter {
-	/**
-	 * Instantiates the main properties
-	 * Creates the clasess' controllers path, router instance and
-	 * a resource map
-	 */
-	constructor() {
-		this.controllerPath = './controllers/';
-		this.router = Express.Router();
-		this.resources = [];
-	}
+  #controllerPath = './controllers/'
+  #version = ''
+  #router = Express.Router();
+  #resources = []
 
-	/**
-	 * Creates a get route
-	 * @param  {String} path      The URL path
-	 * @param  {String} shorthand The shorthand controller#method notation
-	 * @param  {Object} options   Routing options
-	 */
-	get(path, shorthand, options) {
-		options = this._parseOptions(shorthand, options);
-		this.router.get(path, this._callAction(options));
-	}
+  // getters & setters
+  get version() {
+    return this.#version
+  }
 
-	/**
-	 * Creates a post route
-	 * @param  {String} path      The URL path
-	 * @param  {String} shorthand The shorthand controller#method notation
-	 * @param  {Object} options   Routing options
-	 */
-	post(path, shorthand, options) {
-		options = this._parseOptions(shorthand, options);
-		this.router.post(path, this._callAction(options));
-	}
+  get controllerPath() {
+    let tmp = `${this.#controllerPath}${this.#version}`    
+    if (tmp == './controllers')  { return './controllers/' }
 
-	/**
-	 * Creates a put route
-	 * @param  {String} path      The URL path
-	 * @param  {String} shorthand The shorthand controller#method notation
-	 * @param  {Object} options   Routing options
-	 */
-	put(path, shorthand, options) {
-		options = this._parseOptions(shorthand, options);
-		this.router.put(path, this._callAction(options));
-	}
+    if (tmp.charAt(tmp.length - 1) === '/') { tmp = tmp.slice(0, -1) }
+    return `${tmp}/`
+  }
 
-	/**
-	 * Creates a delete route
-	 * @param  {String} path      The URL path
-	 * @param  {String} shorthand The shorthand controller#method notation
-	 * @param  {Object} options   Routing options
-	 */
-	delete(path, shorthand, options) {
-		options = this._parseOptions(shorthand, options);
-		this.router.delete(path, this._callAction(options));
-	}
+  get router() {
+    return this.#router
+  }
 
-	/**
-	 * Creates a routing resource
-	 * @param  {String} resourceName The name of the resource
-	 * @param  {String} controller   The name of the controller, if different from the resource
-	 */
-	resource(resourceName, controller) {
-		let resourceRouter = Express.Router();
-		if(!controller) controller = resourceName;
+  set version(value) {
+    if (!value || (value && value.trim() === '')) { return this.#version = '' }
+    this.#version = `/${value}`
+  }
 
-		this.resources.push(resourceName);
+  set controllerPath(value) {
+    if (!value || (value && value.trim() === '')) { return this.#controllerPath = './controllers' }
+    this.#controllerPath = value
+  }
 
-		resourceRouter.get('/',
-			this._callAction({ controller: controller, method: 'index', middleware: []}));
-		resourceRouter.get('/new',
-			this._callAction({ controller: controller, method: 'new', middleware: [] }));
-		resourceRouter.post('/',
-			this._callAction({ controller: controller, method: 'create', middleware: [] }));
-		resourceRouter.get('/:id',
-			this._callAction({ controller: controller, method: 'show', middleware: [] }));
-		resourceRouter.get('/:id/edit',
-			this._callAction({ controller: controller, method: 'edit', middleware: [] }));
-		resourceRouter.put('/:id',
-			this._callAction({ controller: controller, method: 'update', middleware: [] }));
-		resourceRouter.delete('/:id',
-			this._callAction({ controller: controller, method: 'destroy', middleware: [] }));
+  /**
+   * Creates a get route
+   * @param  {String} path      The URL path
+   * @param  {String} shorthand The shorthand controller#method notation
+   * @param  {Object} options   Routing options
+   */
+  get(path, shorthand, options) {
+    options = this._parseOptions(shorthand, options);
+    this.router.get(`${this.version}${path}`, this._callAction(options));
+  }
 
-		this.router.use('/'+resourceName, resourceRouter);
-	}
+  /**
+   * Creates a post route
+   * @param  {String} path      The URL path
+   * @param  {String} shorthand The shorthand controller#method notation
+   * @param  {Object} options   Routing options
+   */
+  post(path, shorthand, options) {
+    options = this._parseOptions(shorthand, options);    
+    this.router.post(`${this.version}${path}`, this._callAction(options));
+  }
 
-	/**
-	 * Mounts a middleware function to all routes
-	 * @param  {Function} middleware The express-compliant middleware
-	 */
-	mountMiddleware(middleware) {
-		this.router.use(middleware)
-	}
+  /**
+   * Creates a put route
+   * @param  {String} path      The URL path
+   * @param  {String} shorthand The shorthand controller#method notation
+   * @param  {Object} options   Routing options
+   */
+  put(path, shorthand, options) {
+    options = this._parseOptions(shorthand, options);
+    this.router.put(`${this.version}${path}`, this._callAction(options));
+  }
 
-	/**
-	 * Returns the express.Router() instance to be used by express
-	 * @return {express.Router()}
-	 */
-	draw() {
-		return this.router;
-	}
+  /**
+   * Creates a delete route
+   * @param  {String} path      The URL path
+   * @param  {String} shorthand The shorthand controller#method notation
+   * @param  {Object} options   Routing options
+   */
+  delete(path, shorthand, options) {
+    options = this._parseOptions(shorthand, options);
+    this.router.delete(`${this.version}${path}`, this._callAction(options));
+  }
 
-	pathToResource(path, action, parameters) {
-		if(typeof action === 'object') {
-			parameters = action;
-			action = undefined;
-		}
+  /**
+   * Creates a routing resource
+   * @param  {String} resourceName The name of the resource
+   * @param  {String} controller   The name of the controller, if different from the resource
+   */
+  resource(resourceName, controller) {
+    let resourceRouter = Express.Router();
+    if(!controller) controller = resourceName;
 
-		if(this.resources.indexOf(path) == -1) // Error if resource has not been defined
-			throw new Error('RESOURCE_DOESNT_EXIST');
+    this.resources.push(resourceName);
 
-		if(action === undefined || (action === 'index' && parameters == undefined))
-			return '/'+path;
+    resourceRouter.get('/',
+      this._callAction({ controller: controller, method: 'index', middleware: []}));
+    resourceRouter.get('/new',
+      this._callAction({ controller: controller, method: 'new', middleware: [] }));
+    resourceRouter.post('/',
+      this._callAction({ controller: controller, method: 'create', middleware: [] }));
+    resourceRouter.get('/:id',
+      this._callAction({ controller: controller, method: 'show', middleware: [] }));
+    resourceRouter.get('/:id/edit',
+      this._callAction({ controller: controller, method: 'edit', middleware: [] }));
+    resourceRouter.put('/:id',
+      this._callAction({ controller: controller, method: 'update', middleware: [] }));
+    resourceRouter.delete('/:id',
+      this._callAction({ controller: controller, method: 'destroy', middleware: [] }));
 
-		else if((action === undefined && parameters && parameters.id) || (action === 'show' && parameters.id))
-			return '/'+path+'/'+parameters.id;
+    this.router.use('/'+resourceName, resourceRouter);
+  }
 
-		else if(action === 'new')
-			return '/'+path+'/new';
+  /**
+   * Mounts a middleware function to all routes
+   * @param  {Function} middleware The express-compliant middleware
+   */
+  mountMiddleware(middleware) {
+    this.router.use(middleware)
+  }
 
-		else if(action === 'edit' && parameters && parameters.id)
-			return '/'+path+'/'+parameters.id+'/edit';
-	}
+  /**
+   * Returns the express.Router() instance to be used by express
+   * @return {express.Router()}
+   */
+  draw() {
+    return this.router;
+  }
 
-	/**
-	 * Sets the routers' controllers path
-	 * @param {String} path The path to find the controllers
-	 */
-	setControllerPath(path) {
-		this.controllerPath = path;
-	}
+  pathToResource(path, action, parameters) {
+    if(typeof action === 'object') {
+      parameters = action;
+      action = undefined;
+    }
 
-	/* private */
+    if(this.resources.indexOf(path) == -1) // Error if resource has not been defined
+      throw new Error('RESOURCE_DOESNT_EXIST');
 
-	/**
-	 * Parse the shorthand and route options into a standard object,
-	 * to be used by the routes.
-	 * @param  {String} shorthand The shorthand controller#method notation
-	 * @param  {Object} options   Routing options
-	 * @return {Object}
-	 */
-	_parseOptions(shorthand, options = {}) {
-		if(typeof shorthand === 'object') options = shorthand;
-		else {
-			let shorthandOptions = shorthand.split('#');
-			options.controller = shorthandOptions[0];
-			options.method = shorthandOptions[1];
-		}
+    if(action === undefined || (action === 'index' && parameters == undefined)) 
+      return `${this.version}/${path}`
 
-		if(!options.middleware)
-			options.middleware = []
-		if(!Array.isArray(options.middleware))
-			options.middleware = [options.middleware]
+    else if((action === undefined && parameters && parameters.id) || (action === 'show' && parameters.id))
+      return `${this.version}/${path}/${parameters.id}`
 
-		return options;
-	}
+    else if(action === 'new') 
+      return `${this.version}/${path}/new`
 
-	/**
-	 * Calls the method of the controller, on the context of the instance,
-	 * passing express' req and res params
-	 * @param  {Object}   options Routing options
-	 * @return {Function}
-	 */
-	_callAction(options) {
-		const controller = new(require(this.controllerPath + options.controller + '.js'))();
-		const method = controller[options.method];
-		const call = (req, res, next) => { method.call(controller, req, res, next) }
-		return [...options.middleware, call];
-	}
+    else if(action === 'edit' && parameters && parameters.id) 
+      return `${this.version}/${path}/${parameters.id}/edit`
+  }
+
+
+  /* private */
+
+  /**
+   * Parse the shorthand and route options into a standard object,
+   * to be used by the routes.
+   * @param  {String} shorthand The shorthand controller#method notation
+   * @param  {Object} options   Routing options
+   * @return {Object}
+   */
+  _parseOptions(shorthand, options = {}) {
+    if(typeof shorthand === 'object') options = shorthand;
+    else {
+      let shorthandOptions = shorthand.split('#');
+      options.controller = shorthandOptions[0];
+      options.method = shorthandOptions[1];
+    }
+
+    if(!options.middleware)
+      options.middleware = []
+    if(!Array.isArray(options.middleware))
+      options.middleware = [options.middleware]
+
+    return options;
+  }
+
+  /**
+   * Calls the method of the controller, on the context of the instance,
+   * passing express' req and res params
+   * @param  {Object}   options Routing options
+   * @return {Function}
+   */
+  _callAction(options) {    
+    try {
+      const controller = new(require(this.controllerPath + options.controller + '.js'))();
+      const method = controller[options.method];
+      
+      const call = (req, res, next) => { method.call(controller, req, res, next) }
+      return [...options.middleware, call];
+    } catch (err) {
+      console.log(err);
+      throw err
+    }
+  }
 }
 
 /**
@@ -177,9 +197,9 @@ class IkeRouter {
  * available even outside the main routing file in the host app.
  */
 const instance = new IkeRouter();
-function singleton(controllerPath) {
-	if(controllerPath) instance.setControllerPath(controllerPath);
-	return instance;
-}
 
-module.exports = singleton;
+module.exports = (controllerPath, version = '') => {  
+  if(version && version.trim() !== '') instance.version = version;
+  if(controllerPath) instance.controllerPath = controllerPath
+  return instance;
+};
